8f590a292a6101e6cf9b0c99e1fdc1bb4a0b3b8b
Andrew Fox <afox@F-C02ZG0WWLVDP.local>
2019-11-14 13:48:10 -0800

    Add gatsby-plugin-robots-txt and configure to disallow all

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,6 +44,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://famsf-signage.netlify.com',
+        policy: [{ userAgent: '*', disallow: "/"}]
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
